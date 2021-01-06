f39ae9345ff1985eaf31270418b60e6a79de32f7
Waldo <Waldo@KA-LW10-3371357.kabk.nl>
2021-01-06 22:45:46 +0100

    Back to working version

--- PSAdvent21_Day3_Part1.ps1
+++ PSAdvent21_Day3_Part1.ps1
@@ -4,43 +4,29 @@ $XPos = 0
 $Tree = 0
 $Clear = 0
 
-$XMoves = 1,3,5,7,1
-YMoves =
-
-$HashMove = 
-
-foreach ($Xmove in $XMoves) 
+Foreach ($Line in $SlopeInput)
 {
-    
-    Foreach ($Line in $SlopeInput)
+    if ($Line[$XPos] -eq "#")
     {
-        if ($Line[$XPos] -eq "#")
-        {
-            Write-Verbose "$($Line[$XPos])"
-            Write-Verbose "HIT A TREE!"
-            $Tree++
-        }
-        else 
-        {
-            Write-Verbose "$($Line[$Xpos])"
-            Write-Verbose "all is clear"
-            $Clear++
-        }
-
-        $XPos += $XMove
-        if ($XPos -ge 31)
-        {
-            $Xpos -= 31
-
-        }
-        Write-Verbose "New Xpos = $Xpos"
+        $Line[$XPos]
+        Write-Verbose "HIT A TREE!"
+        $Tree++
+    }
+    else 
+    {
+        $Line[$Xpos]
+        Write-Verbose "all is clear"
+        $Clear++
     }
 
-    Write-Output  "Done with $XMove"
-    Write-Output "Trees hit: $Tree"
-    Write-Output "Clear: $Clear"
+    $XPos += 3
+    if ($XPos -ge 31)
+    {
+        $Xpos -= 31
+        
+    }
+    Write-Verbose "New Xpos = $Xpos"
 }
-
 Write-Output  "Done"
 Write-Output "Trees hit: $Tree"
 Write-Output "Clear: $Clear"
